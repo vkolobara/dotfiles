e33b0d43234683f79650a268b2f7cb178f494685
Vinko Kolobara <kolobara.vinko@gmail.com>
2024-09-21 11:45:36 +0200

    feat: merge

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -199,14 +199,46 @@ return require("lazy").setup({
         opts = {},
     },
     {
-        "ariel-frischer/bmessages.nvim",
-        event = "CmdlineEnter",
-        opts = {}
-    },
-    {
         "karb94/neoscroll.nvim",
         config = function()
             require('neoscroll').setup({})
         end
     },
+    {
+        "folke/trouble.nvim",
+        opts = {}, -- for default options, refer to the configuration section for custom setup.
+        cmd = "Trouble",
+        keys = {
+            {
+                "<leader>xx",
+                "<cmd>Trouble diagnostics toggle<cr>",
+                desc = "Diagnostics (Trouble)",
+            },
+            {
+                "<leader>xX",
+                "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+                desc = "Buffer Diagnostics (Trouble)",
+            },
+            {
+                "<leader>cs",
+                "<cmd>Trouble symbols toggle focus=false<cr>",
+                desc = "Symbols (Trouble)",
+            },
+            {
+                "<leader>cl",
+                "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+                desc = "LSP Definitions / references / ... (Trouble)",
+            },
+            {
+                "<leader>xL",
+                "<cmd>Trouble loclist toggle<cr>",
+                desc = "Location List (Trouble)",
+            },
+            {
+                "<leader>xQ",
+                "<cmd>Trouble qflist toggle<cr>",
+                desc = "Quickfix List (Trouble)",
+            },
+        },
+    }
 })
