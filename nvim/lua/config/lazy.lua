0fb4667cfcc52109af9d2e413fbf520ab60e6abf
Vinko Kolobara <kolobara.vinko@gmail.com>
2025-02-26 11:00:03 +0100

    feat: add octo

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -262,6 +262,19 @@ return require("lazy").setup(
                 { "<leader>gl", function() Snacks.lazygit.log() end, desc = "Git Log" },
             }
 
+        },
+        {
+          'pwntester/octo.nvim',
+          requires = {
+            'nvim-lua/plenary.nvim',
+            'nvim-telescope/telescope.nvim',
+            -- OR 'ibhagwan/fzf-lua',
+            -- OR 'folke/snacks.nvim',
+            'nvim-tree/nvim-web-devicons',
+          },
+          config = function ()
+            require"octo".setup()
+          end
         }
     }
 )
